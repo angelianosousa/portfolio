a83300d8ebc3713ab84c4182aca5853cc011ea96
Angeliano <angeliano.sousa@gmail.com>
2022-02-22 11:37:52 -0300

    Adicionado estilo a views do devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( 
-  user_devise.js user_devise.css
+  devise.css devise.js
   site.css site.js
 )
 
