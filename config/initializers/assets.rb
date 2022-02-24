67f6a9b7500d732e58df1bf277102a6f261f9c36
Angeliano <angeliano.sousa@gmail.com>
2022-02-23 21:15:51 -0300

    Agora o heroku pega

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( 
+  backoffice.css backoffice.js
   devise.css devise.js
   site.css site.js
 )
